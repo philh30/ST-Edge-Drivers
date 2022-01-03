7174d0de0f27f1dbec57ea8ed2976810e7f74939
philh30 <45735120+philh30@users.noreply.github.com>
2022-01-03 09:40:22 -0500

    initial commit

---
+++ GE-Jasco/ge-zigbee-switch/src/init.lua
@@ -0,0 +1,79 @@
+-- Copyright 2021 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local capabilities = require "st.capabilities"
+local ZigbeeDriver = require "st.zigbee"
+local defaults = require "st.zigbee.defaults"
+local clusters = require "st.zigbee.zcl.clusters"
+local ElectricalMeasurement = clusters.ElectricalMeasurement
+local SimpleMetering = clusters.SimpleMetering
+local constants = require "st.zigbee.constants"
+
+--- @param driver ZigbeeDriver The current driver running containing necessary context for execution
+--- @param device st.zigbee.Device The device this message was received from containing identifying information
+local do_configure = function(driver, device)
+  device:refresh()
+  device:configure()
+
+  -- Additional one time configuration
+  if device:supports_capability(capabilities.energyMeter) or device:supports_capability(capabilities.powerMeter) then
+    -- Divisor and multipler for EnergyMeter
+    device:send(ElectricalMeasurement.attributes.ACPowerDivisor:read(device))
+    device:send(ElectricalMeasurement.attributes.ACPowerMultiplier:read(device))
+    -- Divisor and multipler for PowerMeter
+    device:send(SimpleMetering.attributes.Divisor:read(device))
+    device:send(SimpleMetering.attributes.Multiplier:read(device))
+  end
+end
+
+--- Fix for an error in the default handler for InstantaneousDemand attribute on SimpleMetering cluster
+---
+--- @param driver ZigbeeDriver The current driver running containing necessary context for execution
+--- @param device st.zigbee.Device The device this message was received from containing identifying information
+--- @param value st.zigbee.data_types.Int24 the value of the instantaneous demand
+--- @param zb_rx st.zigbee.ZigbeeMessageRx the full message this report came in
+local function instantaneous_demand_handler(driver, device, value, zb_rx)
+  local raw_value = value.value
+  --- demand = demand received * Multipler/Divisor
+  local multiplier = device:get_field(constants.SIMPLE_METERING_MULTIPLIER_KEY) or 1
+  local divisor = device:get_field(constants.SIMPLE_METERING_DIVISOR_KEY) or 1
+  raw_value = raw_value * multiplier/divisor*1000
+  device:emit_event_for_endpoint(zb_rx.address_header.src_endpoint.value, capabilities.powerMeter.power({value = raw_value, unit = "W" }))
+end
+
+---- Driver template config
+local driver_template = {
+  supported_capabilities = {
+    capabilities.switch,
+    capabilities.switchLevel,
+    capabilities.powerMeter,
+    capabilities.energyMeter,
+    capabilities.refresh
+  },
+  lifecycle_handlers = {
+    doConfigure = do_configure,
+  },
+---[[
+  zigbee_handlers = {
+    attr = {
+      [SimpleMetering.ID] = {
+        [SimpleMetering.attributes.InstantaneousDemand.ID] = instantaneous_demand_handler,
+      }
+    }
+  },
+}
+
+defaults.register_for_default_handlers(driver_template, driver_template.supported_capabilities)
+local ge_switch = ZigbeeDriver("ge-zigbee-switch", driver_template)
+ge_switch:run()
