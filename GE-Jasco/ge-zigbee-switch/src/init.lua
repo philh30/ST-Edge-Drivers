2d7a2c2b4e5490db28d99a1f2f4662b23b2f951c
philh30 <45735120+philh30@users.noreply.github.com>
2022-01-04 22:37:58 -0500

    add dimming options

--- GE-Jasco/ge-zigbee-switch/src/init.lua
+++ GE-Jasco/ge-zigbee-switch/src/init.lua
@@ -18,7 +18,12 @@ local defaults = require "st.zigbee.defaults"
 local clusters = require "st.zigbee.zcl.clusters"
 local ElectricalMeasurement = clusters.ElectricalMeasurement
 local SimpleMetering = clusters.SimpleMetering
+local Level = clusters.Level
+local OnOff = clusters.OnOff
 local constants = require "st.zigbee.constants"
+local log = require "log"
+local utils = require "st.utils"
+local delay_send = require "delay_send"
 
 --- @param driver ZigbeeDriver The current driver running containing necessary context for execution
 --- @param device st.zigbee.Device The device this message was received from containing identifying information
@@ -35,6 +40,73 @@ local do_configure = function(driver, device)
     device:send(SimpleMetering.attributes.Divisor:read(device))
     device:send(SimpleMetering.attributes.Multiplier:read(device))
   end
+end
+
+--- @param driver ZigbeeDriver The current driver running containing necessary context for execution
+--- @param device st.zigbee.Device The device this message was received from containing identifying information
+--- @param args table The capability command table
+local function info_changed(driver, device, args)
+  if device.preferences['defaultLevel'] then
+    local dim_raw = device.preferences['defaultLevel']
+    local max_level = device.preferences['maxLevel'] or 254
+    local min_level = device.preferences['minLevel'] or 0
+    min_level = (min_level < max_level) and min_level or 0
+    dim_raw = (dim_raw == 0) and 255 or math.floor(min_level+dim_raw*(max_level-min_level)/100)
+    device:send(Level.attributes.OnLevel:write(device,dim_raw))
+    device:send(Level.attributes.OnLevel:read(device))
+  end
+  device:refresh()
+end
+
+local function raw_to_level(device,raw)
+  local max_level = device.preferences['maxLevel'] or 254
+  local min_level = device.preferences['minLevel'] or 0
+  min_level = (min_level < max_level) and min_level or 0
+  local level = (raw==0) and 0 or math.floor(min_level+raw*(max_level-min_level)/100)
+  return level
+end
+
+--- This will send the move to level with on off command to the level control cluster
+---
+--- @param driver ZigbeeDriver The current driver running containing necessary context for execution
+--- @param device st.zigbee.Device The device this message was received from containing identifying information
+--- @param command table The capability command table
+local function set_level(driver, device, command)
+  local curr_level = device:get_latest_state(command.component,capabilities.switchLevel.ID,capabilities.switchLevel.level.NAME)
+  local curr_state = device:get_latest_state(command.component,capabilities.switch.ID,capabilities.switch.switch.NAME)
+  local raw = command.args.level
+  local level = raw_to_level(device,raw)
+  local rate = command.args.rate or device.preferences['levelChangeTime'] or 0xFFFF
+  local scaleRate = device.preferences['levelChangeScaling'] == '1'
+  local startAtZero = device.preferences['startLevel'] == '0'
+  local cmd = {}
+  if startAtZero and (curr_state == 'off') then
+    table.insert(cmd,Level.server.commands.MoveToLevel(device, 1, 0))
+    curr_level = 0
+  end
+  if scaleRate then
+    local change = curr_level and math.abs(curr_level - raw) or 100
+    rate = (rate == 0xFFFF) and 0xFFFF or math.floor(rate*change/100)
+  end
+  table.insert(cmd,Level.server.commands.MoveToLevelWithOnOff(device, level, rate))
+  if curr_state == 'off' then
+    table.insert(cmd,clusters.OnOff.server.commands.On(device))
+  end
+  delay_send(device,cmd,0.5)
+end
+
+--- This converts the Uint8 value from 0-254 to SwitchLevel.level(0-100)
+---
+--- @param driver Driver The current driver running containing necessary context for execution
+--- @param device st.zigbee.Device The device this message was received from containing identifying information
+--- @param value st.zigbee.data_types.Uint8 the value of the current level attribute of the level control cluster
+--- @param zb_rx st.zigbee.ZigbeeMessageRx the full message this report came in
+local function level_attr_handler(driver, device, value, zb_rx)
+  local max_level = device.preferences['maxLevel'] or 254
+  local min_level = device.preferences['minLevel'] or 0
+  min_level = (min_level < max_level) and min_level or 0
+  local level = math.floor(((utils.clamp_value(value.value,min_level,max_level)-min_level)/(max_level-min_level)* 100) + 0.5)
+  device:emit_event_for_endpoint(zb_rx.address_header.src_endpoint.value, capabilities.switchLevel.level(level))
 end
 
 --- Fix for an error in the default handler for InstantaneousDemand attribute on SimpleMetering cluster
@@ -63,13 +135,22 @@ local driver_template = {
   },
   lifecycle_handlers = {
     doConfigure = do_configure,
+    infoChanged = info_changed,
   },
   zigbee_handlers = {
     attr = {
+      [Level.ID] = {
+        [Level.attributes.CurrentLevel.ID] = level_attr_handler
+      },
       [SimpleMetering.ID] = {
         [SimpleMetering.attributes.InstantaneousDemand.ID] = instantaneous_demand_handler,
       }
     }
+  },
+  capability_handlers = {
+    [capabilities.switchLevel.ID] = {
+      [capabilities.switchLevel.commands.setLevel.NAME] = set_level,
+    },
   },
 }
 
