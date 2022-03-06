287fd4872920b51de4b5bedb3de7e90e3ecbe2d3
philh30 <45735120+philh30@users.noreply.github.com>
2022-03-06 17:41:08 -0500

    add fingerprint

--- GE-Jasco/ge-zwave-switch/src/preferences.lua
+++ GE-Jasco/ge-zwave-switch/src/preferences.lua
@@ -136,7 +136,7 @@ local devices = {
     MATCHING_MATRIX = {
       mfrs = {0x0039, 0x0063},
       product_types = {0x4952},
-      product_ids = {0x3135, 0x3137, 0x3139, 0x3231, 0x3237, 0x3238},
+      product_ids = {0x3135, 0x3136, 0x3137, 0x3139, 0x3231, 0x3237, 0x3238},
     },
     PARAMETERS = GE_SCENE.PARAMETERS,
     BUTTONS = GE_SCENE.BUTTONS,
