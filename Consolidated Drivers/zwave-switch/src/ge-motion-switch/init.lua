d6b67db54a443f9712884a6ebbc1d134ea000100
Corey Stup <CoreyStup@gmail.com>
2023-07-22 17:18:06 -0400

    bring driver code over from ge-switch and ge-motion-switch
    Bring over most of the current handlers from GE Switch/GE Motion Switch into Consolidated.

--- Consolidated Drivers/zwave-switch/src/ge-motion-switch/init.lua
+++ Consolidated Drivers/zwave-switch/src/ge-motion-switch/init.lua
@@ -16,7 +16,10 @@ local capabilities = require "st.capabilities"
 local cc = require "st.zwave.CommandClass"
 local ZwaveDriver = require "st.zwave.driver"
 local defaults = require "st.zwave.defaults"
-local log = require "log"
+--- @type st.zwave.constants
+local constants = require "st.zwave.constants"
+--- @type st.utils
+local utils = require "st.utils"
 --- @type st.zwave.CommandClass.Association
 local Association = (require "st.zwave.CommandClass.Association")({ version=2 })
 --- @type st.zwave.CommandClass.Configuration
@@ -80,15 +83,46 @@ end
 
 --- @param driver st.zwave.Driver
 --- @param device st.zwave.Device
+--- @param cmd st.zwave.CommandClass.Basic.Set
+local function basic_set(driver,device,cmd)
+  local evt = (cmd.args.value == 0) and capabilities.button.button.down() or capabilities.button.button.up()
+  evt.state_change = true
+  device:emit_event(evt)
+end
+
+--- @param driver st.zwave.Driver
+--- @param device st.zwave.Device
 --- @param cmd st.zwave.CommandClass.Basic.Report
 local function basic_report(driver,device,cmd)
   local event
-  if cmd.args.value == SwitchBinary.value.OFF_DISABLE then
-    event = capabilities.switch.switch.off()
+  local value = nil
+  if cmd.args.target_value ~= nil then
+    value = cmd.args.target_value
+    if cmd.args.target_value == SwitchBinary.value.OFF_DISABLE then
+      event = capabilities.switch.switch.off()
+    else
+      event = capabilities.switch.switch.on()
+    end
   else
-    event = capabilities.switch.switch.on()
+    value = cmd.args.value
+    if cmd.args.value == SwitchBinary.value.OFF_DISABLE then
+      event = capabilities.switch.switch.off()
+    else
+      event = capabilities.switch.switch.on()
+    end
   end
   device:emit_event_for_endpoint(cmd.src_channel, event)
+  event = nil
+  if value ~= nil and value > 0 then
+    if value == 99 or value == 0xFF then
+      value = 100
+    end
+    event = capabilities.switchLevel.level(value)
+  end
+
+  if event ~= nil then
+    device:emit_event_for_endpoint(cmd.src_channel, event)
+  end
 end
 
 --- @param driver st.zwave.Driver
@@ -164,6 +198,85 @@ local function defaultlevel_handler(driver,device,command)
   device:send(Configuration:Get({parameter_number = 17}))
 end
 
+local function get_delay(device,dimmingDuration,distance)
+  local delay = constants.MIN_DIMMING_GET_STATUS_DELAY -- delay in seconds
+  if (device.preferences or {}).dimStepsZwave and (device.preferences or {}).dimTimeZwave and dimmingDuration == 'default' and not ((device.preferences or {}).dimRate == '0' and distance < 99) then
+    local steps = math.ceil(distance/device.preferences.dimStepsZwave)
+    local sec_per_step = device.preferences.dimTimeZwave/100
+    delay = math.max(steps * sec_per_step + constants.DEFAULT_POST_DIMMING_DELAY, delay)
+  elseif dimmingDuration ~= 'default' then
+    delay = math.max(dimmingDuration + constants.DEFAULT_POST_DIMMING_DELAY, delay)
+  end
+  return delay
+end
+
+local function get_distance(device,command)
+  local level = utils.round(command.args.level)
+  level = utils.clamp_value(level, 1, 99)
+  local current_level = device:get_latest_state(command.component,'switchLevel','level')
+  local current_switch = device:get_latest_state(command.component,'switch','switch')
+  local distance = 100
+  if current_switch == 'off' then
+    distance = level
+  elseif current_level then
+    current_level = utils.clamp_value(current_level,1,99)
+    distance = math.abs(current_level - level)
+  end
+  return level, distance
+end
+
+local function switch_set_helper(driver, device, value, command)
+  local set = Basic:Set({ value = value })
+  local get
+  local delay = constants.DEFAULT_GET_STATUS_DELAY
+  if device:is_cc_supported(cc.SWITCH_BINARY) then
+    get = SwitchBinary:Get({})
+  elseif device:is_cc_supported(cc.SWITCH_MULTILEVEL) then
+    local dimmingDuration = command.args.rate or 'default'
+    delay = get_delay(device,dimmingDuration,99)
+    get = SwitchMultilevel:Get({})
+  else
+    get = Basic:Get({})
+  end
+  device:send_to_component(set, command.component)
+  local query_device = function()
+    device:send_to_component(get, command.component)
+  end
+  device.thread:call_with_delay(delay, query_device)
+end
+
+local function on_handler(driver, device, command)
+  switch_set_helper(driver, device, SwitchBinary.value.ON_ENABLE, command)
+end
+
+local function off_handler(driver, device, command)
+  switch_set_helper(driver, device, SwitchBinary.value.OFF_DISABLE, command)
+end
+
+local function switch_level_handler(driver, device, command)
+  local set
+  local get
+  local level, distance = get_distance(device,command)
+  local dimmingDuration = command.args.rate or 'default' -- dimming duration in seconds
+  local delay = get_delay(device,dimmingDuration,distance) -- delay in seconds
+  if device:is_cc_supported(cc.SWITCH_MULTILEVEL) then
+    get = SwitchMultilevel:Get({})
+    if dimmingDuration == "default" then
+      set = Basic:Set({ value=level })
+    else
+      set = SwitchMultilevel:Set({ value=level, duration=dimmingDuration })
+    end
+  elseif device:is_cc_supported(cc.BASIC) then
+    get = Basic:Get({})
+    set = Basic:Set({ value=level})
+  end
+  device:send_to_component(set, command.component)
+  local query_level = function()
+    device:send_to_component(get, command.component)
+  end
+  device.thread:call_with_delay(delay, query_level)
+end
+
 local ge_motion = {
   zwave_handlers = {
     [cc.CONFIGURATION] = {
@@ -171,6 +284,7 @@ local ge_motion = {
     },
     [cc.BASIC] = {
       [Basic.REPORT] = basic_report,
+      [Basic.SET] = basic_set,
     },
   },
   supported_capabilities = {
@@ -202,6 +316,13 @@ local ge_motion = {
     },
     [DefaultLevel.ID] = {
       [DefaultLevel.commands.setDefaultLevel.NAME] = defaultlevel_handler,
+    },
+    [capabilities.switch.ID] = {
+      [capabilities.switch.commands.on.NAME] = on_handler,
+      [capabilities.switch.commands.off.NAME] = off_handler,
+    },
+    [capabilities.switchLevel.ID] = {
+      [capabilities.switchLevel.commands.setLevel.NAME] = switch_level_handler,
     },
   },
   NAME = "ge zwave motion switch",
